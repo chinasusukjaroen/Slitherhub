1e029a01d4b35f58b0e911b001a718ec1da74026
Nantapoparm <Nantapop.cho@dome.tu.ac.th>
2026-05-01 04:31:02 +0700

    final_ultimate_final_last_one_is_final_final_last_final_final_final

--- backend/config.py
+++ backend/config.py
@@ -25,5 +25,6 @@ MOODLE_VIEW_URL = os.getenv("MOODLE_VIEW_URL", "https://courses.cs.tu.ac.th/mod/
 # ─── CORS ─────────────────────────────────────────────────
 ALLOWED_ORIGINS = [
     "http://127.0.0.1:5500",
-    "http://localhost:5500"
+    "http://localhost:5500",
+    "http://34.224.26.199:5500"
 ]
