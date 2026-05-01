d52a506e2b71b95950e7430add8a0f2458544d00
Nantapoparm <Nantapop.cho@dome.tu.ac.th>
2026-05-01 14:46:03 +0700

    final_ultimate_final_last_one_is_final_final_last_final_final_final_lasteverything_final_lastfinal_last_last_of_last_test_last_last_of_final_adjust_requirefile_deploy_front_final

--- frontend/javascript/config.js
+++ frontend/javascript/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
 BACKEND_API_URL: "https://slitherhub.onrender.com/api",
-ALLOWED_ORIGINS: ["http://127.0.0.1:5500"]
+ALLOWED_ORIGINS: ["https://slitherhub-1.onrender.com"]
 };
